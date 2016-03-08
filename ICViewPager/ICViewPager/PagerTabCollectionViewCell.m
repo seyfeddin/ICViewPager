b6e8457b094407ba4c9b5e0fb3722c893c4a90ea
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2016-03-08 17:37:29 +0200

    adds collectionview cells

---
+++ ICViewPager/ICViewPager/PagerTabCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PagerTabCollectionViewCell.m
+//  Pods
+//
+//  Created by Seyfeddin Bassarac on 05/11/15.
+//
+//
+
+#import "PagerTabCollectionViewCell.h"
+
+@implementation PagerTabCollectionViewCell
+
+@end
